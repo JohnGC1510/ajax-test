0e9ebfa3549839b52174523dccf4cd82141ae865
JohnGC1510 <johngc1510@gmail.com>
2020-08-03 09:02:49 +0000

    Data is now retrieved and displayed in a table format

--- main.js
+++ main.js
@@ -16,19 +16,40 @@ function getData(type, cb){
     /*this.status represents http status code, 200 is request succeeded. Another example of a status code is 301,401, 404 and numerous other error codes*/
     xhr.onreadystatechange = function(){ /* onreadystatechange calls te function when the readystate property changes, xhr.open() and xhr.send() both change the readystate property and hence the function is called*/
     if(this.readyState == 4 && this.status == 200){
-       cb(JSON.parse(this.responseText)); /*change inner html to responeText taken from our xhr object, JSON.parse coverts from a strong to an object rather than a string that looks like an object*/
+       cb(JSON.parse(this.responseText)); /*change inner html to responeText taken from our xhr object, JSON.parse coverts from a string to an object rather than a string that looks like an object*/
     }
 };
 }
+/*Function to get move through the key values of each key pair (eg. key = name, value = Luke SkyWalker) and to return an array of the table headers for a specific object
+ Creating an array through the keys should stop us having to call specific data types for each of our objects on the star wars site as we will have access to each key of every object*/
+function getTableHeaders(obj){
+    var tableHeaders = [];
+    Object.keys(obj).forEach(function(key){
+       tableHeaders.push('<td>' + key + '</td>'); 
+    });
+
+    return '<tr>' + tableHeaders + '</tr>'
+}
 
 function writeToDocument(type){
+    
     var el = document.getElementById("data");
     el.innerHTML="";
     getData(type,function(data){
+        var tableRows = []; //set data in a table to make it easier to display
         data = data.results; /*console.dir(data) used to show the format of data in the object and it found an array called results, which contains an array of 10 items */
+        var tableHeaders = getTableHeaders(data[0]);
         data.forEach(function(item){
-             el.innerHTML += "<p>" + item.name + "</p>";
-        })
+            var dataRow = []; //Empty array for each data set 
+            Object.keys(item).forEach(function(key) {
+                var rowData = item[key].toString();
+                var truncatedData = rowData.substring(0,15);
+                dataRow.push('<td>' + truncatedData + '</td>'); //item[key] returns the value stored in they key rather than just the key name
+            });
+            tableRows.push("<tr>" + dataRow + "</tr>");
+        });
+
+        el.innerHTML = '<table>' + tableHeaders + tableRows + '</table>';
         /*change inner html to responeText taken from our xhr object*/
-    })
+    });
 }
