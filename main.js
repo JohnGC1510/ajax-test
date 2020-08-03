05cca38c56afa7c207ed96cf8c9c192ab16710f3
JohnGC1510 <johngc1510@gmail.com>
2020-08-03 09:37:09 +0000

    removed commas form display

--- main.js
+++ main.js
@@ -64,7 +64,7 @@ function writeToDocument(url){
             tableRows.push("<tr>" + dataRow + "</tr>");
         });
 
-        el.innerHTML = '<table>' + tableHeaders + tableRows + '</table>' + pagination;
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`.replace(/,/g, "");
         /*change inner html to responeText taken from our xhr object*/
     });
 }
